959d6b042e8f5dde6a0f985b76fad9283892a295
Dennis <deweis@gmx.ch>
2018-09-04 23:34:02 +0200

    Add some more plants

--- script.js
+++ script.js
@@ -24,23 +24,59 @@ const plants = [
     latin: 'Amaranthus retroflexus',
     soil: [1, 3],
     img: 'AmaranthusRetroflexus.jpg',
-    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Amaranthus_retroflexus_full1.jpg', },
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Amaranthus_retroflexus_full1.jpg',
+    desc: 'Der Zurückgebogene Amarant (Amaranthus retroflexus), auch Zurückgekrümmter Fuchsschwanz oder Rauhaariger Amarant genannt, ist eine Pflanzenart innerhalb der Familie der Fuchsschwanzgewächse (Amaranthaceae). Er ist in Mitteleuropa ein häufiges, wärmeliebendes „Unkraut“ in Mais- und Zuckerrübenfeldern, in Haus- und Gemüsegärten sowie in Weinbergen.', },
   { name: 'Gemeiner Odermenning',
     latin: 'Agrimonia eupatoria',
     soil: [1],
     img: 'AgrimoniaEupatoria.jpg',
-    imgSrc: 'https://de.wikipedia.org/wiki/Gemeiner_Odermennig#/media/File:Agrimonia_eupatoria_04.jpg',},
+    imgSrc: 'https://de.wikipedia.org/wiki/Gemeiner_Odermennig#/media/File:Agrimonia_eupatoria_04.jpg',
+    desc: 'Der Gewöhnliche Odermennig (Agrimonia eupatoria), auch Gemeiner Odermennig, Ackerkraut oder Kleiner Odermennig genannt, ist eine Pflanzenart in der Unterfamilie der Rosoideae innerhalb der Familie der Rosengewächse (Rosaceae). Sie ist in Eurasien weitverbreitet.', },
   { name: 'Acker-Gauchheil',
     latin: 'Anagallis arvensis',
     soil: [15],
     img: 'AnagallisArvensis.jpg',
-    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/1/15/AnagallisArvensis.jpg', },
-  { name: 'Salz-Aster', latin: 'Aster tripolium', soil: [18] },
-  { name: 'Kanadisches Berufskraut', latin: 'Erigeron canadensis', soil: [2] },
-  { name: 'Gewöhnliches Greiskraut', latin: 'Senecio vulgaris', soil: [3, 17] },
-  { name: 'Adlerfarn', latin: 'Pteridium aquilinum', soil: [7, 9, 14] },
-  { name: 'Schwarzes Bilsenkraut', latin: 'Hyoscvamus niger', soil: [15] },
-  { name: 'Gemeiner Beifuß', latin: 'Artemisia vulgaris', soil: [4] },
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/1/15/AnagallisArvensis.jpg',
+    desc: 'Der Acker-Gauchheil (Anagallis arvensis), auch Roter Gauchheil, Nebelpflanze, Weinbergsstern oder Wetterkraut genannt, ist eine Pflanzenart aus der Gattung Gauchheil (Anagallis) in der Unterfamilie der Myrsinengewächse (Myrsinaoideae) innerhalb der Familie der Primelgewächse (Primulaceae). Er gilt im Ackerbau als Unkraut, weil er schwach giftig in all seinen Teilen ist, vor allem in der Wurzel.', },
+  { name: 'Salz-Aster',
+    latin: 'Aster tripolium',
+    soil: [18],
+    img: 'AsterTripolium.jpg',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Strandasters.jpg',
+    desc: 'Die Strand-Aster (Tripolium pannonicum, Syn.: Aster tripolium L.), auch Salz-Aster und Pannonien-Salzaster genannt, ist eine Pflanzenart in der Familie der Korbblütler (Asteraceae). Sie gehört zu den sehr auffälligen Pflanzenarten auf Salzböden.', },
+  { name: 'Kanadisches Berufskraut',
+    latin: 'Erigeron canadensis',
+    soil: [2],
+    img: 'ErigeronCanadensis.jpg',
+    imgSrc: 'https://de.wikipedia.org/wiki/Kanadisches_Berufkraut#/media/File:Conyza_canadense_002.JPG',
+    desc: 'Das Kanadische Berufkraut (Conyza canadensis), auch Katzenschweif und Weiße Dürrwurz genannt, ist eine Pflanzenart aus der Familie der Korbblütler (Asteraceae). Es wird auch oft unter dem wissenschaftlichen Namen Erigeron canadensis der Gattung Erigeron geführt.', },
+  { name: 'Gewöhnliches Greiskraut',
+    latin: 'Senecio vulgaris',
+    soil: [3, 17],
+    img: 'SenecioVulgaris.jpg',
+    imgSrc: 'https://de.wikipedia.org/wiki/Gew%C3%B6hnliches_Greiskraut#/media/File:20150418Senecio_vulgaris6.jpg',
+    desc: 'Das Gewöhnliche Greiskraut (Senecio vulgaris), auch Gemeines Greiskraut oder Gemeines Kreuzkraut genannt, ist eine der häufigsten Arten aus der Gattung der Greiskräuter (Senecio) innerhalb der Familie der Korbblütler (Asteraceae).', },
+  { name: 'Adlerfarn',
+    latin: 'Pteridium aquilinum',
+    soil: [7, 9, 14],
+    img: 'PteridiumAquilinum.jpg',
+    imgSrc: 'https://de.wikipedia.org/wiki/Adlerfarn#/media/File:Pteridium_aquilinum_(habitus).jpg',
+    desc: 'Der Adlerfarn (Pteridium aquilinum) ist ein weltweit verbreiteter, auffälliger Farn. Die Farne sind eine Gruppe von Gefäßsporenpflanzen, die die Schwestergruppe der Samenpflanzen bilden.',
+    photographer: 'Hans Hillewaert', },
+  { name: 'Schwarzes Bilsenkraut',
+    latin: 'Hyoscyamus niger',
+    soil: [15],
+    img: 'HyoscyamusNiger.jpg',
+    imgSrc: 'https://de.wikipedia.org/wiki/Schwarzes_Bilsenkraut#/media/File:Hyoscyamus_niger_0003.JPG',
+    desc: 'Das Schwarze Bilsenkraut (Hyoscyamus niger) ist eine Pflanze aus der Gattung der Bilsenkräuter aus der Familie der Nachtschattengewächse (Solanaceae). Schwarzes Bilsenkraut wächst in Schuttunkrautgesellschaften, an Wegrändern, Mauern usw. Es bevorzugt frische, nährstoff- und stickstoffreiche Sand- oder Lehmböden.',
+    photographer: 'H. Zell', },
+  { name: 'Gemeiner Beifuß',
+    latin: 'Artemisia vulgaris',
+    soil: [4],
+    img: 'ArtemisiaVulgaris.jpg',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/ArtemisiaVulgaris.jpg',
+    desc: 'Der Beifuß oder Gemeine Beifuß (Artemisia vulgaris), auch Gewürzbeifuß oder Gewöhnlicher Beifuß genannt, ist eine Pflanzenart aus der Gattung Artemisia in der Familie der Korbblütler (Asteraceae, veraltet Compositae). Auf nährstoffreichen Böden, vor allem Ruderalfluren kommt der Beifuß wild vor.',
+    photographer: 'Christian Fischer', },
   { name: 'Gemeine Wegwarte', latin: 'Cichorium intybus', soil: [3, 12, 17] },
   { name: 'Gewöhnliche Vogelmiere', latin: 'Stellaria media', soil: [3, 17] },
   { name: 'Huflattich', latin: 'Tussilago farfara', soil: [2, 12, 14] },
@@ -91,17 +127,18 @@ function showPlants() {
   const plantsList = document.getElementById('plants-list');
 
   /* add one card per entry in plants object */
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 9; i++) {
     const card = document.createElement('div');
     card.setAttribute('class', 'col-md-6 col-lg-4');
     card.innerHTML = `
             <div class="card">
               <img class="card-img-top"
-                   src="img/500x666/${plants[i].img}"
+                   src="img/500x500/${plants[i].img}"
                    alt="${plants[i].name}">
               <div class="card-body" id="${plants[i].name}">
                 <h5 class="card-title">${plants[i].name}</h5>
-                <p class="card-text"><em>${plants[i].latin}</em></p>
+                <h6><em>${plants[i].latin}</em></h6>
+                <p class="card-text">${plants[i].desc}</p>
               </div>
             </div>`;
     plantsList.appendChild(card);
