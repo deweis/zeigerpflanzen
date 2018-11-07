4e0480a557052b67b51661c632f5a08b79c28aa3
Dennis <deweis@gmx.ch>
2018-11-07 11:18:40 +0100

    Cache the app shell and return respective requests from the cache

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('sw-static').then(function(cache) {
+      /*
+      Cache the app shell: The application shell is the frame of the
+      application, I.e. the core of the app. Means, the static content like
+      the html file, css, js and images.
+      From a developers POV these should not change that often.
+      The dynamic content will be cached to, but is not part of the app
+     shell.
+     */
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/zeigerpflanzen/',
+        '/zeigerpflanzen/index.html',
+        '/zeigerpflanzen/script.js',
+        '/zeigerpflanzen/styles.css',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,5 +30,17 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log('[Service Worker] Fetching data ...', event);
-  event.respondWith(fetch(event.request)); // just return the fetch request
+  /* event.respondWith allows us to overwrite the data which gets sent back */
+  event.respondWith(
+    caches
+      .match(event.request) // check if the request is in cache
+      .then(function(response) {
+        if (response) {
+          // if there we return the request (from the cache)
+          return response;
+        } else {
+          return fetch(event.request); // else we fetch the data. I.e. continue with the network request.
+        }
+      })
+  );
 });
